9011ee05bacf4db70bdc536f20be2776120eb8f4
subtain96 <awaisiofficial0096@gmail.com>
2025-07-11 06:58:45 -0700

    Services page complete

--- portfolio-react/src/Components/Services/Services.jsx
+++ portfolio-react/src/Components/Services/Services.jsx
@@ -1,6 +1,8 @@
     import React from 'react'
     import './Services.css'
     import theme_pattern from '../../assets/theme_pattern.svg'
+    import ServicesData from '../../assets/services_data'
+    import arrow_icon from "../../assets/arrow_icon.svg"
     const Services = () => {
       return (
         <div className='services'>
@@ -9,6 +11,18 @@
                 <img src={theme_pattern} alt="" />
             </div>
         <div className="services-container">
+          {ServicesData.map((service , index)=>{
+            return <div key={index} className='services-format'>
+              <h3>{service.s_no}</h3>
+              <h2>{service.s_name}</h2>
+              <p>{service.s_desc}</p>
+              <div className="services-readmore">
+                <p>Read More</p>
+                <img src={arrow_icon} alt="" />
+              </div>
+            </div>
+
+          })}
             </div>  
         </div>
       )
